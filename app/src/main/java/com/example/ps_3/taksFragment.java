abe0323dec99034701df21a047d7d00fc7e822e7
Dawid Pieciul <pieciul.dawid@gmail.com>
2021-11-03 15:33:29 +0100

    Do kroku 9

---
+++ app/src/main/java/com/example/ps_3/taksFragment.java
@@ -0,0 +1,62 @@
+package com.example.ps_3;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class taksFragment extends Fragment {
+
+    private EditText nameField;
+    private Button dateButton;
+    private CheckBox doneCheckBox;
+    private Task task;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_task, container, false);
+
+        nameField = view.findViewById(R.id.task_name);
+        nameField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                task.setName(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        dateButton.setText(task.getDate().toString());
+        dateButton.setEnabled(false);
+
+        doneCheckBox.setChecked(task.isDone());
+        doneCheckBox.setOnCheckedChangeListener((buttonView, isChecked)-> { //todo dal Maćka
+            task.setDone(isChecked);
+        });
+        return view;
+    }
+}
